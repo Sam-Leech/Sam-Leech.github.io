92b7777881c65ab5e10b2624af8e3f1b9d9f575f
Sam-Leech <saml@frexperts.co.uk>
2026-07-31 10:09:43 +0100

    Login for downloads

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVqaht2BVQgVkZFcwMZ3t34DQeA2uFYuU",
+  authDomain: "fre-tools.firebaseapp.com",
+  projectId: "fre-tools",
+  storageBucket: "fre-tools.firebasestorage.app",
+  messagingSenderId: "723940111430",
+  appId: "1:723940111430:web:50cb5685913167f0dc4647"
+};
+
+export const app = initializeApp(firebaseConfig);
